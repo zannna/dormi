85784341ce728310a5a0c3528034de7a5787ce73
Anna Zielińska <lananahb@gmail.com>
2023-04-27 00:00:43 +0200

    removed unnecessary references to authorities table

--- dormi-backend/src/main/java/com/example/wdpai2backend/repository/UserRepository.java
+++ dormi-backend/src/main/java/com/example/wdpai2backend/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.example.wdpai2backend.repository;
 
 import com.example.wdpai2backend.entity.AppUser;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<AppUser, Long> {
+    @Query("SELECT a FROM AppUser a  join fetch a.authorities  WHERE a.email = ?1")
     Optional<AppUser> findByEmail(String email);
+    @Query( "SELECT a.dormitory.id_dorm FROM AppUser a WHERE a.email = ?1" )
+    Long findIdDormitoryByEmail(String email);
+
 }

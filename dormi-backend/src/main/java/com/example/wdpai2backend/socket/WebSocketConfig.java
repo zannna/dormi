4cfe3873e3307ecb08f182de0c4cfb0f6749ade4
Anna Zielińska <lananahb@gmail.com>
2023-06-05 04:03:04 +0200

    added webSocket configuration

---
+++ dormi-backend/src/main/java/com/example/wdpai2backend/socket/WebSocketConfig.java
@@ -0,0 +1,37 @@
+package com.example.wdpai2backend.socket;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000");
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
+
+
+    }
+
+
+}
